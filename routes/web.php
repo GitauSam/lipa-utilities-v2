0c4dbb5695af86941f671283837ae8eb34273402
GitauSam <njorogegitausamuel@gmail.com>
2021-04-12 13:48:48 +0300

    rolls back protecting all routes with https

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(array('https'), function() {
+// Route::group(array('https'), function() {
     Route::get('/', function () {
         return view('welcome');
     });
@@ -59,4 +59,4 @@ Route::group(array('https'), function() {
         });
 
     });
-});
+// });
