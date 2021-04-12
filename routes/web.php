9bfd694c4a9b5eff7f5189db468515469600ab68
GitauSam <njorogegitausamuel@gmail.com>
2021-04-12 13:46:51 +0300

    protects all routes with https

--- routes/web.php
+++ routes/web.php
@@ -17,44 +17,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Route::get('/welcome/beta', function () {
-//     return view('welcome-beta');
-// });
-
-
-Route::middleware(['auth:sanctum', 'verified'])->group(function() {
-
-    // Utility routes
-    Route::resource('utility', UserUtilityController::class);
-
-    // Utility payments routes
-    Route::resource('payments', UtilityPaymentController::class);
-
-    // Utility Payment routes
-    Route::get('utility/pay/mpesa/{id}', [MpesaController::class, 'showLipaNaMpesa'])
-        ->name('utility.lipa-na-mpesa.get');
-    Route::post('utility/pay/mpesa', [MpesaController::class, 'executeLipaNaMpesaTransaction'])
-        ->name('utility.lipa-na-mpesa');
-
-    // Admin routes
-    Route::group(['middleware' => ['role:super-admin|moderator']], function() {
-        
-        // Utility routes
-        Route::prefix('admin')->group(function() {
-                Route::name('admin.')->group(function() {
-                        Route::resource('utility', UtilityController::class);
-                });
-
-                // Dashboard
-        Route::get('/dashboard', function() {
-                        return view('dashboard');
-                })->name('dashboard');
-        });
-        
+Route::group(array('https'), function() {
+    Route::get('/', function () {
+        return view('welcome');
     });
 
+    // Route::get('/welcome/beta', function () {
+    //     return view('welcome-beta');
+    // });
+
+
+    Route::middleware(['auth:sanctum', 'verified'])->group(function() {
+
+        // Utility routes
+        Route::resource('utility', UserUtilityController::class);
+
+        // Utility payments routes
+        Route::resource('payments', UtilityPaymentController::class);
+
+        // Utility Payment routes
+        Route::get('utility/pay/mpesa/{id}', [MpesaController::class, 'showLipaNaMpesa'])
+            ->name('utility.lipa-na-mpesa.get');
+        Route::post('utility/pay/mpesa', [MpesaController::class, 'executeLipaNaMpesaTransaction'])
+            ->name('utility.lipa-na-mpesa');
+
+        // Admin routes
+        Route::group(['middleware' => ['role:super-admin|moderator']], function() {
+            
+            // Utility routes
+            Route::prefix('admin')->group(function() {
+                    Route::name('admin.')->group(function() {
+                            Route::resource('utility', UtilityController::class);
+                    });
+
+                    // Dashboard
+            Route::get('/dashboard', function() {
+                            return view('dashboard');
+                    })->name('dashboard');
+            });
+            
+        });
+
+    });
 });
